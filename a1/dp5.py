674696a338c5d00a3f54dcf0ed05d0040277299e
Zeyu Zhang <zzhan785@uwo.ca>
2022-02-19 18:26:43 -0500

    w

--- a1/dp5.py
+++ a1/dp5.py
@@ -12,23 +12,27 @@ if __name__ == "__main__":
     A = np.ones(N,dtype=np.float32) 
     B = np.ones(N,dtype=np.float32)
 
-    total_time=[]
+    total_time=0.0
+    half_total=0.0
     
     # iterate by measurement_num times 
     for i in range(measurement_num):
         # compute execution time 
-        start=time.monotonic() 
+        start=time.monotonic_ns() 
         np.dot(A,B)
-        end=time.monotonic()   
+        end=time.monotonic_ns() 
+ 
         # add to total_time 
-        total_time.append(end-start)
+        total_time+=end-start
+        if i>=measurement_num/2:
+            half_total+=end-start
     
-    average_time=sum(total_time)/measurement_num
-    second_half_average_time=sum(total_time[int(measurement_num/2):])/(measurement_num/2)
-    bandwidth=(sys.getsizeof(A)+sys.getsizeof(B))/second_half_average_time*(10**-9)
-    flops=2*N/second_half_average_time*(10**-9)
+    
+    second_half_average_time=half_total/(measurement_num/2)
+
+    bandwidth=(sys.getsizeof(A)+sys.getsizeof(B))/second_half_average_time
+    flops=2*N/second_half_average_time
     print("dp 5")
     # N: 1000000 <T>: 9.999999 sec B: 9.999 GB/sec F: 9.999 FLOP/sec
-    print("N: {};<T>: {} sec; B: {} GB/sec; F: {} GFLOP/sec".format(N,second_half_average_time,bandwidth,flops))
-    # print("number of vectors: {}; number of measurements: {}; average execution time: {}".format(N,measurement_num,average_time));
+    print("N: {};<T>: {} sec; B: {} GB/sec; F: {} GFLOP/sec".format(N,second_half_average_time*pow(10,-9),bandwidth,flops))    # print("number of vectors: {}; number of measurements: {}; average execution time: {}".format(N,measurement_num,average_time));
 
